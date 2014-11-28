5d357b5cb1bc639208f45d0bdec148543802f557
Code Division <codedivision@11th.ventures>
2014-11-28 15:21:00 +0800

    master

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,9 +1,8 @@
 require 'faker'
-require 'uri'
 
 class Url < ActiveRecord::Base
-
-  before_save :createShortURL
+  validates :short_url, uniqueness: true
+  before_create :createShortURL
 
   def createShortURL
     self.short_url = "http://localhost:9393/" << Faker::Base.regexify("[A-Za-z0-9]{5}")
